1bd0f0692106f86d7337d85cebbff95d2286b232
Vadim <vadimz026@gmail.com>
2019-11-10 20:44:23 +0300

    10.11.19 point 20:45

--- app/src/main/java/com/example/neurophproject/BJActivity.java
+++ app/src/main/java/com/example/neurophproject/BJActivity.java
@@ -1,10 +1,40 @@
 package com.example.neurophproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class BJActivity extends AppCompatActivity {
+/*
+    FloatingActionButton fab =
+            (FloatingActionButton)findViewById(R.id.fab);
+
+fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText input = (EditText)findViewById(R.id.input);
+
+            // Read the input field and push a new instance
+            // of ChatMessage to the Firebase database
+            FirebaseDatabase.getInstance()
+                    .getReference()
+                    .push()
+                    .setValue(new ChatMessage(input.getText().toString(),
+                            FirebaseAuth.getInstance()
+                                    .getCurrentUser()
+                                    .getDisplayName())
+                    );
+
+            // Clear the input
+            input.setText("");
+        }
+    });
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
