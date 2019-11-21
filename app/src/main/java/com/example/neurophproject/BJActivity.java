9f04113ab0a2182a0275878789d4060d5ad95f46
Vadim <vadimz026@gmail.com>
2019-11-21 23:10:16 +0300

    21.11.19 point 23:09

--- app/src/main/java/com/example/neurophproject/BJActivity.java
+++ app/src/main/java/com/example/neurophproject/BJActivity.java
@@ -1,44 +1,43 @@
 package com.example.neurophproject;
 
+import android.app.ActionBar;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class BJActivity extends AppCompatActivity {
-/*
-    FloatingActionButton fab =
-            (FloatingActionButton)findViewById(R.id.fab);
-
-fab.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            EditText input = (EditText)findViewById(R.id.input);
-
-            // Read the input field and push a new instance
-            // of ChatMessage to the Firebase database
-            FirebaseDatabase.getInstance()
-                    .getReference()
-                    .push()
-                    .setValue(new ChatMessage(input.getText().toString(),
-                            FirebaseAuth.getInstance()
-                                    .getCurrentUser()
-                                    .getDisplayName())
-                    );
-
-            // Clear the input
-            input.setText("");
-        }
-    });
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_job);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                .parseColor("#FFDEAD")));
+        EditText input = findViewById(R.id.input);
+        LinearLayout forMessage = findViewById(R.id.for_message);
+
+        fab.setOnClickListener(view->{
+            String text = input.getText().toString();
+            if (!text.isEmpty()) {
+                TextView tv = new TextView(this.getApplicationContext());
+                tv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                tv.setText(text);
+                tv.setBackgroundResource(R.drawable.back_ground_message);
+                forMessage.addView(tv);
+                input.setText(null);
+            }
+        });
+
+
+
     }
 }
